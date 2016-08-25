f689de810a76a8ee36c616117b9fd685d7f42ea5
David Stone <papyrus8712@gmail.com>
2016-08-25 14:07:20 +0100

    Update shared.lua

--- zombies/gamemode/shared.lua
+++ zombies/gamemode/shared.lua
@@ -1,6 +1,7 @@
 GM.Name = "Zombies. A Half Life 2 based zombie gamemode."
-GM.Author = "Papyrus"
+GM.Author = "Papyrus. Inspiration taken from JetBoom's Zombie Survival gamemode and <CODE BLUE>'s tutorials."
 function GM:Initialize()
 	self.BaseClass.Initialize(self)
 end
+team.SetUp(1, "TEAM_SURV", Color(255,0,255), true)
 util.PrecacheModel("models/player/kleiner.mdl")
